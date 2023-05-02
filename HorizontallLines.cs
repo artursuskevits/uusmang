20070425762ed1c407a5618bc4b98f7587fdb641
opilane <opilane@HP-CZC2349HTN>
2023-05-02 11:17:54 +0300

    half 6 lesson

---
+++ HorizontallLines.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uusmang
+{
+    internal class HorizontallLines
+    {
+        List<Point> plist;
+
+        public HorizontallLines()
+        {
+            plist = new List<Point>();
+            Point p1 = new Point(4, 4, '*');
+            Point p2 = new Point(5, 4, '*');
+            Point p3 = new Point(6, 4, '*');
+            plist.Add(p1);
+            plist.Add(p2);
+            plist.Add(p3);
+        }
+        public void Drow()
+        {
+            foreach (Point p in plist)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
--- uusmang.cs
+++ uusmang.cs
@@ -19,29 +19,15 @@ namespace uusmang
            
 
             Point p2 = new Point(4,5,'#');
-            
             p2.Draw();
 
-            List<int> numlist = new List<int>();
-            numlist.Add(0);
-            numlist.Add(1);
-            numlist.Add(2);
-            numlist.Add(3);
-            numlist.Add(4);
-            int x = numlist[0];
-            int y = numlist[1];
-            int z = numlist[2];
+            HorizontallLines line = new HorizontallLines();
+            line.Drow();
 
-            Console.WriteLine();
-            foreach (int i in numlist)
-            {
-                Console.WriteLine(i);
-            }
-            numlist.Remove(4);
-            List<Point> plist = new List<Point>();
-            plist.Add(p1);
-            plist.Add(p2);  
+           
+           
+
         }
-    
+
     }
 }
