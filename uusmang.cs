514e4a17a551610cb17a19fcae2b49fd34ea5bbb
opilane <opilane@HP-CZC2349HTN>
2023-05-02 10:06:57 +0300

    Add project files.

---
+++ uusmang.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace uusmang
+{
+    public class uusmang
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("gaga");
+        }
+    }
+}
